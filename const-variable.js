64c0a70e6f095beb2884b80f767faa34ea8121ed
ranjeet <ranjeet.kumar@srijan.net>
2017-08-14 11:06:24 +0530

    Declare const variable and re-assignement

---
+++ const-variable.js
@@ -0,0 +1,13 @@
+/**
+ * In ES6 a new key const is used to declare the variable, constant variable can not be re-assigned
+ */
+
+const a = 5;
+console.log(a);
+//output 5
+console.log('-------------------');
+
+//Re-assigning
+a = 7;
+console.log(a);
+//output It will give the TypeError
