cb82adb970ee6aa964d8db13e4944b74d2243085
ranjeet <ranjeet.kumar@srijan.net>
2017-08-14 13:18:55 +0530

    Destructuring Assignment array

---
+++ destructuring-array.js
@@ -0,0 +1,17 @@
+/**
+ * @file destructuring Assigment Array
+ *
+ * Descructuring Assignment Array allows extract data from Array to the the variables
+ *
+ * @see example
+ */
+
+"use strict";
+
+let name = ['Ranjeet', 'kumar', 'Mahto'];
+
+// assign array elements to the variables
+let  [firstName, middleName, lastName] = name;
+
+console.log(firstName, middleName, lastName);
+//output Ranjeet kumar mahto
